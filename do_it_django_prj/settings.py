36fbb5b0b5232c5dc0e7d6a6a10030537d80a600
splanky0314 <splanky0314@gmail.com>
2023-01-14 22:03:41 +0900

    포스트 목록 페이지에 대표 이미지 출력하기

--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -118,6 +119,9 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '_media') # 프로젝트 폴더 아래에 _media폴더 생성 & 그 안에 저장 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
