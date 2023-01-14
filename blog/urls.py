e3343139f8f003256296214da7306376df9df6d1
splanky0314 <splanky0314@gmail.com>
2023-01-14 20:30:21 +0900

    CDV로 블로그 포스트 목록 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index), # views.py에 있는 index() 함수 실행
+    path('', views.PostList.as_view()), # views.py에 있는 index() 함수 실행
     path('<int:pk>/', views.single_post_page)
 ]
